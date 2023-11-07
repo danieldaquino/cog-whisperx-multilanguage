ac0dfe4a90d32ce4a54b288a082599de4d560657
Daniel D’Aquino <daniel@daquino.me>
2023-11-07 15:34:16 -0800

    Added support for Spanish and Portuguese

--- predict.py
+++ predict.py
@@ -14,8 +14,15 @@ class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         self.device = "cuda"
-        self.model = whisperx.load_model("medium", self.device, language="en", compute_type=compute_type)
-        self.alignment_model, self.metadata = whisperx.load_align_model(language_code="en", device=self.device)
+        self.model = whisperx.load_model("large", self.device, compute_type=compute_type)
+        self.languages = ["en", "pt", "es"]   # TODO: Add more languages or load dynamically from whisperx. Few languages are supported at the moment to keep memory usage down.
+        self.alignment_models = {}
+        for language_code in self.languages:
+            alignment_model, metadata = whisperx.load_align_model(
+                language_code=language_code,
+                device=self.device
+            )
+            self.alignment_models[language_code] = {"model": alignment_model, "metadata": metadata}
 
     def predict(
         self,
@@ -27,12 +34,19 @@ class Predictor(BasePredictor):
     ) -> str:
         """Run a single prediction on the model"""
         with torch.inference_mode():
-            result = self.model.transcribe(str(audio), batch_size=batch_size) 
+            result = self.model.transcribe(str(audio), batch_size=batch_size)
             # result is dict w/keys ['segments', 'language']
             # segments is a list of dicts,each dict has {'text': <text>, 'start': <start_time_msec>, 'end': <end_time_msec> }
             if align_output:
                 # NOTE - the "only_text" flag makes no sense with this flag, but we'll do it anyway
-                result = whisperx.align(result['segments'], self.alignment_model, self.metadata, str(audio), self.device, return_char_alignments=False)
+                result = whisperx.align(
+                    result['segments'],
+                    self.alignment_models[result['language']]['model'],
+                    self.alignment_models[result['language']]['metadata'],
+                    str(audio),
+                    self.device,
+                    return_char_alignments=False
+                )
                 # dict w/keys ['segments', 'word_segments']
                 # aligned_result['word_segments'] = list[dict], each dict contains {'word': <word>, 'start': <start_time_msec>, 'end': <end_time_msec>, 'score': probability}
                 #   it is also sorted
